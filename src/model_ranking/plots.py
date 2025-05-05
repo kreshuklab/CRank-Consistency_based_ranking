2fcf8e4a5899d73f6ffea906cf96034300fea6da
Joshua Talks <joshua.talks@embl.de>
2025-05-05 11:20:36 +0200

    remove deprecated plot function and rename to plot_multitarget_correlations

--- src/model_ranking/plots.py
+++ src/model_ranking/plots.py
@@ -2,10 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import numpy as np
 from numpy.typing import NDArray
-from typing import Any, Dict, List, Mapping, Sequence, Tuple
-
-
-from model_ranking.results import get_summary_results, results_to_arrays
+from typing import Any, List, Mapping, Sequence, Tuple
 
 
 def plot_aug_sweep(
@@ -140,111 +137,9 @@ def plot_aug_sweep(
     plt.show()
 
 
-def plot_combined_correlations(
-    source_datasets: List[str],
-    target_datasets: List[str],
-    source_models: Dict[str, str],
-    per_target_norms: Mapping[str, List[Tuple[float, float]] | List[None]],
-    selected_augmentations: Dict[str, List[str]],
-    consis_keys: Dict[str, str],
-    result_folder_names: Dict[str, str],
-    perf_key: str,
-    base_result_path: str,
-    save_path: str = "",
-    title: str = "",
-    perf_metric_name: str = "MAP score",
-    consis_metric_name: str = "CTE",
-    invert_consis_metric: bool = True,
-    invert_perf_metric: bool = False,
-    perturbation_key: str = "DO",
-    consis_postfix: str = "median_per_alpha",
-    perf_postfix: str = "median",
-    approach: str = "feature_perturbation_consistency",
-    figsize: Tuple[int, int] = (10, 8),
-    fit_line: bool = True,
-    point_size: int = 500,
-    legend_size: int = 28,
-    fontsize: int = 30,
-):
-    _ = plt.figure(figsize=figsize)
-    colors = [
-        "#377eb8",
-        "#ff7f00",
-        "#4daf4a",
-        "#f781bf",
-        "#a65628",
-        "#984ea3",
-        "#999999",
-        "#e41a1c",
-        "#dede00",
-    ]
-
-    for i, target in enumerate(target_datasets):
-        consis_str, _, NA_perf = get_summary_results(
-            source_data=source_datasets,
-            target_data=[target],
-            source_models=source_models,
-            selected_augmentations=selected_augmentations,
-            selected_norms=per_target_norms,
-            consis_keys=consis_keys,
-            perf_key=perf_key,
-            per_target_norms=True,
-            result_folders=result_folder_names,
-            approach=approach,
-            consis_postfix=consis_postfix,
-            perf_postfix=perf_postfix,
-            base_seg_dir=base_result_path,
-        )
-        consis_scores, NA_perf_scores = results_to_arrays(
-            consis_str,
-            NA_perf,
-            perturbation_key,
-            len(selected_augmentations[perturbation_key]),
-        )
-
-        if invert_consis_metric:
-            consis_scores = 1 - consis_scores
-
-        if invert_perf_metric:
-            NA_perf_scores = 1 - NA_perf_scores
-
-        _ = plt.scatter(
-            consis_scores[:, 0],
-            NA_perf_scores,
-            c=colors[i],
-            label=f"To {target}",
-            s=point_size,
-            alpha=0.6,
-        )
-        if fit_line:
-            # plot dotted line that is fitted to data
-            x = np.linspace(
-                np.min(consis_scores[:, 0]) - 0.01,
-                np.max(consis_scores[:, 0]) + 0.01,
-                100,
-            )
-            y = np.poly1d(np.polyfit(consis_scores[:, 0], NA_perf_scores, 1))(x)
-            _ = plt.plot(x, y, "--", c=colors[i], alpha=1, linewidth=3)
-    plt.grid(alpha=0.6)
-    _ = plt.legend(
-        prop={"size": legend_size}, title="Target dataset", title_fontsize=legend_size
-    )
-    _ = plt.xlim(0.19, 1.05)
-    _ = plt.ylim(0, 1)
-    _ = plt.xlabel(consis_metric_name, fontsize=fontsize)
-    _ = plt.ylabel(perf_metric_name, fontsize=fontsize)
-    _ = plt.xticks(fontsize=fontsize - 5)  # pyright: ignore[reportUnknownVariableType]
-    _ = plt.yticks(fontsize=fontsize - 5)  # pyright: ignore[reportUnknownVariableType]
-    _ = plt.title(title, fontsize=fontsize + 5)
-    if len(save_path) > 0:
-        plt.savefig(save_path)
-
-    plt.show()
-
-
-def plot_combined_correlations2(
-    consis_scores: Dict[str, NDArray[Any]],
-    NA_perf_scores: Dict[str, NDArray[Any]],
+def plot_multitarget_correlations(
+    consis_scores: Mapping[str, NDArray[Any]],
+    NA_perf_scores: Mapping[str, NDArray[Any]],
     save_path: str = "",
     title: str = "",
     perf_metric_name: str = "MAP score",
